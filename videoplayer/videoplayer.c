7720e21f449db0599679ba12d54705d7d2a0ba11
Cxd2014 <374530430@qq.com>
2023-01-17 18:08:06 +0800

    增加文字水印

--- videoplayer/videoplayer.c
+++ videoplayer/videoplayer.c
@@ -9,6 +9,8 @@
 #include <libavutil/imgutils.h>
 #include <libavdevice/avdevice.h>
 #include <libavfilter/avfilter.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
 
 #include "rqueue.h"
 
@@ -28,6 +30,10 @@ typedef struct playerState {
     AVFormatContext *pFormatCtx;
     AVCodecContext *pCodecCtx;
     struct SwsContext *pImgConvertCtx;
+    
+    AVFilterGraph *pfilterGraph;
+    AVFilterContext *pBuffersinkCtx;
+    AVFilterContext *pBuffersrcCtx;
 
     SDL_Thread *readThread;
     rqueue_t *pQueue;
@@ -39,6 +45,7 @@ typedef struct playerState {
     bool isQuit;
 
     bool isPause;
+    bool isNext;
     videoFrame *pPauseFrame;
 
     videoFrame videoBuff[QUEUE_BUFF_LEN];
@@ -94,6 +101,10 @@ int init_ffmpeg(playerState *ps) {
         printf("avcodec_parameters_to_context error!");
         return __LINE__;
     }
+
+    // double frame = (pFormatCtx->duration / (double)AV_TIME_BASE) * av_q2d(pFormatCtx->streams[ps->videoIndex]->avg_frame_rate);
+    // printf("rate num:%d den:%d duration:%ld base:%d frame:%1.0f\n", pFormatCtx->streams[ps->videoIndex]->avg_frame_rate.num,
+    //     pFormatCtx->streams[ps->videoIndex]->avg_frame_rate.den, pFormatCtx->duration, AV_TIME_BASE, frame);
 
     //查找解码器
     const AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
@@ -211,12 +222,100 @@ int render_video_frame(playerState *ps, videoFrame *vFrame) {
     return 0;
 }
 
+int init_filters(playerState *ps, const char *filters_descr)
+{
+    char args[512];
+    int ret = 0;
+    const AVFilter *buffersrc  = avfilter_get_by_name("buffer");
+    const AVFilter *buffersink = avfilter_get_by_name("buffersink");
+    AVFilterInOut *outputs = avfilter_inout_alloc();
+    AVFilterInOut *inputs  = avfilter_inout_alloc();
+    AVRational time_base = ps->pFormatCtx->streams[ps->videoIndex]->time_base;
+    enum AVPixelFormat pix_fmts[] = { AV_PIX_FMT_YUV420P, AV_PIX_FMT_NONE };
+
+    ps->pfilterGraph = avfilter_graph_alloc();
+    if (!outputs || !inputs || !ps->pfilterGraph) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+
+    /* buffer video source: the decoded frames from the decoder will be inserted here. */
+    snprintf(args, sizeof(args),
+            "video_size=%dx%d:pix_fmt=%d:time_base=%d/%d:pixel_aspect=%d/%d",
+            ps->pCodecCtx->width, ps->pCodecCtx->height, ps->pCodecCtx->pix_fmt,
+            time_base.num, time_base.den,
+            ps->pCodecCtx->sample_aspect_ratio.num, ps->pCodecCtx->sample_aspect_ratio.den);
+
+    ret = avfilter_graph_create_filter(&ps->pBuffersrcCtx, buffersrc, "in",
+                                       args, NULL, ps->pfilterGraph);
+    if (ret < 0) {
+        printf("Cannot create buffer source\n");
+        goto end;
+    }
+
+    /* buffer video sink: to terminate the filter chain. */
+    ret = avfilter_graph_create_filter(&ps->pBuffersinkCtx, buffersink, "out",
+                                       NULL, NULL, ps->pfilterGraph);
+    if (ret < 0) {
+        printf("Cannot create buffer sink\n");
+        goto end;
+    }
+
+    ret = av_opt_set_int_list(ps->pBuffersinkCtx, "pix_fmts", pix_fmts,
+                              AV_PIX_FMT_NONE, AV_OPT_SEARCH_CHILDREN);
+    if (ret < 0) {
+        printf("Cannot set output pixel format\n");
+        goto end;
+    }
+
+    /*
+     * Set the endpoints for the filter graph. The ps->pfilterGraph will
+     * be linked to the graph described by filters_descr.
+     */
+
+    /*
+     * The buffer source output must be connected to the input pad of
+     * the first filter described by filters_descr; since the first
+     * filter input label is not specified, it is set to "in" by
+     * default.
+     */
+    outputs->name       = av_strdup("in");
+    outputs->filter_ctx = ps->pBuffersrcCtx;
+    outputs->pad_idx    = 0;
+    outputs->next       = NULL;
+
+    /*
+     * The buffer sink input must be connected to the output pad of
+     * the last filter described by filters_descr; since the last
+     * filter output label is not specified, it is set to "out" by
+     * default.
+     */
+    inputs->name       = av_strdup("out");
+    inputs->filter_ctx = ps->pBuffersinkCtx;
+    inputs->pad_idx    = 0;
+    inputs->next       = NULL;
+
+    if ((ret = avfilter_graph_parse_ptr(ps->pfilterGraph, filters_descr,
+                                    &inputs, &outputs, NULL)) < 0)
+        goto end;
+
+    if ((ret = avfilter_graph_config(ps->pfilterGraph, NULL)) < 0)
+        goto end;
+
+end:
+    avfilter_inout_free(&inputs);
+    avfilter_inout_free(&outputs);
+
+    return ret;
+}
+
 // 从文件中读取视频流,解码后发送到队列中
 int do_read_video(void *arg) {
 
     playerState *ps = arg;
     AVPacket *packet = av_packet_alloc();
     AVFrame *frame = av_frame_alloc(); 
+    AVFrame *filt_frame = av_frame_alloc();
     int frameCount = 0;
     bool readEnd = false;
     int queueIndex = 0;
@@ -246,21 +345,42 @@ int do_read_video(void *arg) {
 
         // 从解码器中循环读取帧数据,直到读取失败
         while (avcodec_receive_frame(ps->pCodecCtx, frame) >= 0) {
-            // 复制帧数据        
-            av_frame_move_ref(ps->videoBuff[queueIndex].frame, frame);
-            ps->videoBuff[queueIndex].isRendered = false;
 
-            av_frame_unref(frame);
-            // 写入环形队列
-            if (rqueue_write(ps->pQueue, &(ps->videoBuff[queueIndex])) != 0) {
-                printf("rqueue_write error %d:%d\n", frameCount, queueIndex);
-                return __LINE__;
+            // 再把帧放到 filter 中，添加文字水印
+            frame->pts = frame->best_effort_timestamp;
+            if (av_buffersrc_add_frame_flags(ps->pBuffersrcCtx, frame, AV_BUFFERSRC_FLAG_KEEP_REF) < 0) {
+                printf("Error while feeding the filtergraph\n");
             }
+
+            while (1) {
+                // 从 filter 中读取帧数据
+                int ret = av_buffersink_get_frame(ps->pBuffersinkCtx, filt_frame);
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+                    break;
+                if (ret < 0) {
+                    printf("av_buffersink_get_frame error:%d\n", ret);
+                    break;
+                }
+                
+                // 复制帧数据        
+                av_frame_move_ref(ps->videoBuff[queueIndex].frame, filt_frame);
+                ps->videoBuff[queueIndex].isRendered = false;
+                av_frame_unref(filt_frame);
+
+                // 写入环形队列
+                if (rqueue_write(ps->pQueue, &(ps->videoBuff[queueIndex])) != 0) {
+                    printf("rqueue_write error %d:%d\n", frameCount, queueIndex);
+                    return __LINE__;
+                }
+            }
+
 
             frameCount++;
             queueIndex++;
             if (queueIndex == QUEUE_BUFF_LEN)
                 queueIndex = 0;
+
+            av_frame_unref(frame);
         }
 
         if (readEnd) {
@@ -270,6 +390,7 @@ int do_read_video(void *arg) {
 
     av_packet_free(&packet);
     av_frame_free(&frame);
+    av_frame_free(&filt_frame);
     printf("read file end, write frame count to queue: %d\n", frameCount);
     return 0;
 }
@@ -305,6 +426,15 @@ void do_exit(playerState *ps) {
     exit(0);
 }
 
+void free_pause_frame(playerState *ps) {
+    if (ps->pPauseFrame != NULL) {
+        ps->renderFrameCount++;
+        ps->pPauseFrame->isRendered = true;
+        av_frame_unref(ps->pPauseFrame->frame);
+        ps->pPauseFrame = NULL;
+    }
+}
+
 void event_loop(playerState *ps) {
     SDL_Event event;
     while (1)
@@ -317,7 +447,14 @@ void event_loop(playerState *ps) {
                  switch (event.key.keysym.sym) {
                     case SDLK_SPACE:
                         ps->isPause = !(ps->isPause); // 暂停，开启播放
+                        ps->isNext = false;
                         printf("space key kickdown isPause:%d\n", ps->isPause);
+                        break;
+                    case SDLK_RIGHT:
+                        if (ps->isPause) {
+                            ps->isNext = true; // 暂停状态下才能逐帧播放
+                            printf("right key kickdown isNext:%d\n", ps->isNext);
+                        }
                         break;
                  }
                  break;
@@ -348,6 +485,11 @@ void event_loop(playerState *ps) {
         if (!rqueue_isempty(ps->pQueue)) {
             if (ps->isPause == true) {
 
+                if (ps->isNext == true) {
+                    free_pause_frame(ps);
+                    ps->isNext = false;
+                }
+
                 if (ps->pPauseFrame == NULL) {
                     ps->pPauseFrame = rqueue_read(ps->pQueue);
                 }
@@ -355,17 +497,11 @@ void event_loop(playerState *ps) {
                 render_video_frame(ps, ps->pPauseFrame);
                 SDL_Delay(50);
             } else {
-                
-                if (ps->pPauseFrame != NULL) {
-                    ps->renderFrameCount++;
-                    ps->pPauseFrame->isRendered = true;
-                    av_frame_unref(ps->pPauseFrame->frame);
-                    ps->pPauseFrame = NULL;
-                }
-
+                free_pause_frame(ps);
                 render_video_frame(ps, rqueue_read(ps->pQueue));
             }
         } else {
+            free_pause_frame(ps);
             SDL_Delay(10);
         }
     }
@@ -390,6 +526,7 @@ int init_playerState(playerState *ps) {
     ps->pFileName = NULL;
     ps->isQuit = false;
     ps->isPause = false;
+    ps->isNext = false;
     ps->pPauseFrame = NULL;
 
     ps->renderFrameCount = 0;
@@ -431,6 +568,12 @@ int main(int argc, char *argv[]) {
    
     if (init_ffmpeg(&ps) != 0) {
         printf("init_ffmpeg error\n");
+        return __LINE__;
+    }
+
+    const char *filter_descr = "drawtext=fontsize=60:text='hello world':x=(w-text_w)/2:y=(h-text_h)/2";
+    if (init_filters(&ps, filter_descr) != 0) {
+        printf("init_filters error\n");
         return __LINE__;
     }
 
